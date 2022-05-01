5ea2d3daf482f6149c7ef2c7fa7f5a3f0ae11b8f
Bayan Abuawad <bayana@copyleaks.com>
2022-05-01 12:34:01 +0300

    bump version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022031300;
+$plugin->version = 2022050100;
 $plugin->release = "3.5+";
 $plugin->requires = 2018051700;
 $plugin->component = 'plagiarism_copyleaks';
