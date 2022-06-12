9aa779c329cb751047bc297da3060386e9306eb7
Bayan Abuawad <bayana@copyleaks.com>
2022-06-12 10:51:55 +0300

    Add french translation

---
+++ lang/fr/plagiarism_copyleaks.php
@@ -0,0 +1,100 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'plagiarism_copyleaks', language 'fr', version '3.5'.
+ *
+ * @package     plagiarism_copyleaks
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['claccountconfig'] = 'Configuration du compte Copyleaks';
+$string['claccountkey'] = 'Copyleaks clé';
+$string['claccountsecret'] = 'Copyleaks secrète';
+$string['cladminconfig'] = 'Copyleaks configuration du plugin de plagiat';
+$string['cladminconfigsavesuccess'] = 'Copyleaks les paramètres de plagiat ont été enregistrés avec succès.';
+$string['clallowstudentaccess'] = 'Autoriser les étudiants à accéder aux rapports de plagiat';
+$string['clapisubmissionerror'] = 'Copyleaks a renvoyé une erreur lors de la tentative d\'envoi du fichier pour soumission -';
+$string['clapiurl'] = 'URL de l\'API Copyleaks';
+$string['clcheatingdetected'] = 'Tricherie détectée, ouvrir le rapport pour en savoir plus';
+$string['clcheatingdetectedtxt'] = 'Tricherie détectée';
+$string['cldefaultrepositoriespagetitle'] = 'Paramètres de référentiels par défaut de Copyleaks';
+$string['cldefaultsettings'] = 'Paramètres par défaut du plugin de plagiat Copyleaks';
+$string['cldisabledformodule'] = 'Le plugin Copyleaks est désactivé pour ce module.';
+$string['cldraftsubmit'] = 'Soumettre les fichiers uniquement lorsque les étudiants cliquent sur le bouton Soumettre';
+$string['cldraftsubmit_help'] = 'Cette option n\'est disponible que si "Demander aux étudiants de cliquer sur le bouton d\'envoi" est Oui';
+$string['cleditrepositories'] = 'Edit repositories settings';
+$string['clenable'] = 'Activer Copyleaks';
+$string['clenablecheatdetection'] = 'Activer la détection de triche';
+$string['clenablemodulefor'] = 'Activer les fuites de copie pour {$a}';
+$string['clenablesafesearch'] = 'Activer la recherche sécurisée';
+$string['clfailtoloaddata'] = 'Échec du chargement des données Copyleaks';
+$string['clfailtosavedata'] = 'Échec de l\'enregistrement des données Copyleaks';
+$string['clgenereportimmediately'] = 'Générez des rapports immédiatement';
+$string['clgenereportonduedate'] = 'Générer des rapports à la date d\'échéance';
+$string['clignorequotes'] = 'Ignorer les guillemets';
+$string['clignorereferences'] = 'Ignorer les références';
+$string['clignoreresourcecodecomments'] = 'Ignorer les commentaires du code source';
+$string['clignoreretableofcontents'] = 'Ignorer la table des matières';
+$string['clignoretitles'] = 'Ignorer les titres';
+$string['clinserterror'] = 'Erreur lors de la tentative d\'insertion d\'enregistrements dans la base de données';
+$string['clinvalidkeyorsecret'] = 'Clé ou secret invalide';
+$string['cllogsheading'] = 'Journaux';
+$string['cllogstab'] = 'Journaux';
+$string['clnopageaccess'] = 'Vous n\'avez pas accès à cette page.';
+$string['clopenfullscreen'] = 'Ouvrir en plein écran';
+$string['clopenreport'] = 'Cliquez pour ouvrir le rapport Copyleaks';
+$string['clplagiarised'] = 'Score de similarité';
+$string['clplagiarisefailed'] = 'Manquée';
+$string['clplagiarisequeued'] = 'Analyse de plagiat prévue à {$a}';
+$string['clplagiarisescanning'] = 'Recherche de plagiat...';
+$string['clpluginconfigurationtab'] = 'Configurations';
+$string['clpluginintro'] = 'Le vérificateur de plagiat Copyleaks est une solution complète et précise qui aide les enseignants et les étudiants à vérifier si leur contenu est original.<br>Pour plus d\'informations sur la configuration et l\'utilisation du plugin, veuillez vérifier <a target="_blank" href="https://lti.copyleaks.com/guides/select-moodle-integration">nos guides</a>.</br></br></br>';
+$string['clpoweredbycopyleaks'] = 'Propulsé par Copyleaks';
+$string['clreportgenspeed'] = 'Quand générer un rapport ?';
+$string['clreportpagetitle'] = 'Rapport sur les fuites de copie';
+$string['clrepositoriespagetitle'] = 'Paramètres des référentiels Copyleaks';
+$string['clscaninternaldatabase'] = 'Scanner la base de données interne';
+$string['clscaninternet'] = 'Numériser Internet';
+$string['clscoursesettings'] = 'Paramètres de fuites de copie';
+$string['clsendqueuedsubmissions'] = 'Plugin de plagiat Copyleaks - gérer les fichiers en file d\'attente';
+$string['clstudentdisclosure'] = 'Divulgation des étudiants';
+$string['clstudentdisclosure_help'] = 'Ce texte sera affiché à tous les étudiants sur la page de téléchargement de fichiers.';
+$string['clstudentdisclosuredefault'] = '<span>En soumettant vos fichiers, vous acceptez la politique de confidentialité du service de détection de plagiat</span><a target="_blank" href="https://copyleaks.com/legal/privacypolicy"></a>';
+$string['cltaskfailedconnecting'] = 'la connexion à Copyleaks ne peut pas être établie, erreur : {$a}';
+$string['clupdateerror'] = 'Erreur lors de la tentative de mise à jour des enregistrements dans la base de données';
+$string['clupdatereportscores'] = 'Plugin de plagiat Copyleaks - gérer la mise à jour du score de similarité de la vérification du plagiat';
+$string['copyleaks'] = 'Copyleaks';
+$string['pluginname'] = 'Plugin de plagiat Copyleaks';
+$string['privacy:metadata:core_files'] = 'Copyleaks stocke les fichiers qui ont été téléchargés sur Moodle pour former une soumission Copyleaks.';
+$string['privacy:metadata:plagiarism_copyleaks_client'] = 'Afin de s\'intégrer à un Copyleaks, certaines données utilisateur doivent être échangées avec Copyleaks.';
+$string['privacy:metadata:plagiarism_copyleaks_client:module_creationtime'] = 'L\'heure de création du module est envoyée à Copyleaks à des fins d\'identification.';
+$string['privacy:metadata:plagiarism_copyleaks_client:module_id'] = 'L\'identifiant du module est envoyé à Copyleaks à des fins d\'identification.';
+$string['privacy:metadata:plagiarism_copyleaks_client:module_name'] = 'Le nom du module est envoyé à Copyleaks à des fins d\'identification.';
+$string['privacy:metadata:plagiarism_copyleaks_client:module_type'] = 'Le type de module est envoyé à Copyleaks à des fins d\'identification.';
+$string['privacy:metadata:plagiarism_copyleaks_client:submittion_content'] = 'The submission content is sent to Copyleaks for scan processing.';
+$string['privacy:metadata:plagiarism_copyleaks_client:submittion_name'] = 'Le nom de la soumission est envoyé à Copyleaks à des fins d\'identification.';
+$string['privacy:metadata:plagiarism_copyleaks_client:submittion_type'] = 'Le type de soumission est envoyé à Copyleaks à des fins d\'identification.';
+$string['privacy:metadata:plagiarism_copyleaks_client:submittion_userId'] = 'L\'ID utilisateur de soumission est envoyé à Copyleaks à des fins d\'identification.';
+$string['privacy:metadata:plagiarism_copyleaks_files'] = 'Informations qui relient une soumission Moodle à une soumission Copyleaks.';
+$string['privacy:metadata:plagiarism_copyleaks_files:lastmodified'] = 'Un horodatage indiquant quand l\'utilisateur a modifié sa soumission pour la dernière fois.';
+$string['privacy:metadata:plagiarism_copyleaks_files:similarityscore'] = 'Le score de similarité de la soumission.';
+$string['privacy:metadata:plagiarism_copyleaks_files:submitter'] = 'L\'ID de l\'utilisateur qui a effectué la soumission.';
+$string['privacy:metadata:plagiarism_copyleaks_files:userid'] = 'ID de l\'utilisateur qui est le propriétaire de la soumission.';
