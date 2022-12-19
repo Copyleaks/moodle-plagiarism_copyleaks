b318199fe6eb063a4c99d137614e6bbf61c21f76
Gil Cohen <gilc@copyleaks.com>
2022-12-19 17:00:09 +0200

    1203506798180167 - Rescan internal results - format fixes

--- classes/task/plagiarism_copyleaks_resubmittedreports.php
+++ classes/task/plagiarism_copyleaks_resubmittedreports.php
@@ -1,5 +1,25 @@
 <?php
-
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Copyleaks Plagiarism Plugin - Handle Queued Files
+ * @package   plagiarism_copyleaks
+ * @copyright 2022 Copyleaks
+ * @author    Gil Cohen <gilc@copyleaks.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace plagiarism_copyleaks\task;
 
@@ -38,7 +58,7 @@ class plagiarism_copyleaks_resubmittedreports extends \core\task\scheduled_task 
     $canloadmoredata = true;
 
     while ($canloadmoredata) {
-      $response =  $copyleakscomms->get_resubmit_reports_ids($cursor);
+      $response = $copyleakscomms->get_resubmit_reports_ids($cursor);
       if (!is_object($response) || !isset($response->resubmitted) || count($response->resubmitted) == 0) {
         break;
       }
@@ -46,17 +66,17 @@ class plagiarism_copyleaks_resubmittedreports extends \core\task\scheduled_task 
       $cursor = $response->cursor;
       $resubmittedmodel = $response->resubmitted;
       $canloadmoredata = $response->canLoadMore;
-      $oldIds = array_column($resubmittedmodel, 'oldScanId');
+      $oldids = array_column($resubmittedmodel, 'oldScanId');
 
       $currentdbresults = [];
 
-      // Get all the scans from db with the ids of the 'response' old ids
-      $dbresult = $DB->get_recordset_list('plagiarism_copyleaks_files', 'externalid', $oldIds);
+      /* Get all the scans from db with the ids of the 'response' old ids */
+      $dbresult = $DB->get_recordset_list('plagiarism_copyleaks_files', 'externalid', $oldids);
       if (!isset($dbresult)) {
         break;
       }
 
-      // Getting the result by the consition the all the external ids must contains in $oldIds
+      /* Getting the result by the consition the all the external ids must contains in $oldids */
       foreach ($dbresult as $result) {
         $currentdbresults[] = $result;
       }
@@ -69,13 +89,13 @@ class plagiarism_copyleaks_resubmittedreports extends \core\task\scheduled_task 
       $succeedids = [];
       $idx = 0;
 
-      // For each db result - Replace the new data
+      /* For each db result - Replace the new data */
       foreach ($currentdbresults as $currentresult) {
         if ($currentresult->externalid == null) {
           continue;
         }
 
-        // Get the copyleaks db entity with the old id
+        /* Get the copyleaks db entity with the old id */
         $filtered = array_filter(
           $resubmittedmodel,
           function ($element) use ($currentresult) {
@@ -84,13 +104,14 @@ class plagiarism_copyleaks_resubmittedreports extends \core\task\scheduled_task 
             }
           }
         );
-        $curr =  $filtered[$idx++];
+        /* Even the variable $filtered is assign each time the key will increased in each assignment */
+        $curr = $filtered[$idx++];
 
         if (isset($curr) && $curr != null) {
           $currentresult->externalid = $curr->newScanId;
           $currentresult->similarityscore = $curr->plagiarismScore;
           $currentresult->lastmodified = $timestamp;
-          // Update in the DB
+          /* Update in the DB */
           if (!$DB->update_record('plagiarism_copyleaks_files',  $currentresult)) {
             \plagiarism_copyleaks_logs::add(
               "Update resubmitted failed (old scan id: " . $curr->oldScanId . ", new scan id: "
@@ -102,7 +123,7 @@ class plagiarism_copyleaks_resubmittedreports extends \core\task\scheduled_task 
           }
         }
       }
-      // Send request with ids who successfully changed in moodle db to deletion in the Google data store
+      /* Send request with ids who successfully changed in moodle db to deletion in the Google data store */
       if (count($succeedids) > 0) {
         $copyleakscomms->delete_resubmitted_ids($succeedids);
       }
