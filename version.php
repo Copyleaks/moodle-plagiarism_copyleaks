0864b9175d10ee15609aa8e2a6349d009b685f63
Gil Cohen <gilc@copyleaks.com>
2023-10-25 09:54:35 +0300

    features/1205101169078486-implement-activity-in-lms - text

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023101900;
+$plugin->version = 2023102500;
 $plugin->release = "3.5+";
 $plugin->requires = 2018051700;
 $plugin->component = 'plagiarism_copyleaks';
