1458146487c46b3afc8322e9e8cdecbd4a7d0ff5
Bayan Abuawad <bayana@copyleaks.com>
2023-10-30 09:19:54 +0200

    upgrade.php fix

--- classes/plagiarism_copyleaks_httpclient.class.php
+++ classes/plagiarism_copyleaks_httpclient.class.php
@@ -68,7 +68,7 @@ class plagiarism_copyleaks_http_client {
             )
         );
 
-        $version = 2023051400;
+        $version = 2023103000;
         $headers = (array)[
             'Content-Type' => $contenttype,
             'Plugin-Version' => "$version"
