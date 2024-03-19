0ca9dcfa31d062f573ce0369f529ed0da59536df
Bayan Abuawad <bayana@copyleaks.com>
2024-03-19 14:38:24 +0200

    bump version

--- classes/plagiarism_copyleaks_eventshandler.class.php
+++ classes/plagiarism_copyleaks_eventshandler.class.php
@@ -444,6 +444,7 @@ class plagiarism_copyleaks_eventshandler {
         // Scan immediately.
         $scheduledscandate = 0;
         if ($coursemodule->modname == "quiz") {
+            // We had to add this delay for a bug fix on quiz.
             $scheduledscandate = strtotime('+ 2 minutes');
         } else {
             $scheduledscandate = strtotime('- 1 minutes');
