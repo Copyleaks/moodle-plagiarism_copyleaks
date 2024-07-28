2d45181d8577b3903b3f651baef99e003bc76042
Bayan Abuawad <bayana@copyleaks.com>
2024-07-28 10:34:27 +0300

    bump version

--- classes/plagiarism_copyleaks_httpclient.class.php
+++ classes/plagiarism_copyleaks_httpclient.class.php
@@ -68,7 +68,7 @@ class plagiarism_copyleaks_http_client {
             )
         );
 
-        $version = 2024072500;
+        $version = 2024072800;
         $headers = (array)[
             'Content-Type' => $contenttype,
             'Plugin-Version' => "$version"
