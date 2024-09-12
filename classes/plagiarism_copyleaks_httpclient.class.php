3883ea8a3559f496525bb939b2ab5c15da5f18a7
Gil Cohen <gilc@copyleaks.com>
2024-09-12 16:43:42 +0300

    bump-version

--- classes/plagiarism_copyleaks_httpclient.class.php
+++ classes/plagiarism_copyleaks_httpclient.class.php
@@ -68,7 +68,7 @@ class plagiarism_copyleaks_http_client {
             )
         );
 
-        $version = 2024090900;
+        $version = 2024091200;
         $headers = (array)[
             'Content-Type' => $contenttype,
             'Plugin-Version' => "$version"
