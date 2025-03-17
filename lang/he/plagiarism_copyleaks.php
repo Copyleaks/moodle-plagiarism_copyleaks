d934849be934028aae51bdbcc157b4ae1cdc900b
Shade Amasha <shadea@copyleaks.com>
2025-03-17 16:00:00 +0200

    features/lms-sprint-4/LMS-620-RTL-translations

---
+++ lang/he/plagiarism_copyleaks.php
@@ -0,0 +1,149 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file containes the translations for hebrew.
+ * @package   plagiarism_copyleaks
+ * @copyright 2025 Copyleaks
+ * @author    Shade Amasha <shadeaa@copyleaks.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname']  = 'תוסף גניבה ספרותית Copyleaks';
+$string['copyleaks'] = 'Copyleaks';
+$string['messageprovider:copyleaks_notification'] = "הודעת Copyleaks";
+$string['copyleaks:enable'] = 'הפעלת Copyleaks';
+$string['copyleaks:viewfullreport'] = 'הצגת דוח אינטרנט';
+$string['clstudentdisclosure'] = 'גילוי לסטודנטים';
+$string['clstudentdisclosure_help'] = 'טקסט זה יוצג לכל הסטודנטים בעמוד העלאת הקבצים.';
+$string['clstudentdisclosuredefault']  = '<span>על ידי שליחת הקבצים שלך, אתה מסכים לשירות זיהוי גניבה ספרותית </span><a target="_blank" href="https://copyleaks.com/legal/privacypolicy">מדיניות פרטיות</a>';
+$string['clstudentdagreedtoeula']  = '<span>כבר הסכמת לשירות זיהוי גניבה ספרותית </span><a target="_blank" href="https://copyleaks.com/legal/privacypolicy">מדיניות פרטיות</a>';
+$string['cladminconfigsavesuccess'] = 'הגדרות גניבה ספרותית של Copyleaks נשמרו בהצלחה.';
+$string['clpluginconfigurationtab'] = 'הגדרות';
+$string['cllogstab'] = 'יומנים';
+$string['cladminconfig'] = 'תצורת תוסף גניבה ספרותית של Copyleaks';
+$string['clpluginintro'] = 'בודק הגניבה הספרותית של Copyleaks הוא פתרון מקיף ומדויק המסייע למורים ולתלמידים לבדוק אם התוכן שלהם מקורי.<br>למידע נוסף על אופן ההגדרה והשימוש בתוסף, אנא בדוק את <a target="_blank" href="https://lti.copyleaks.com/guides/select-moodle-integration">המדריכים שלנו</a>.</br></br></br>';
+$string['clenable'] = 'הפעלת Copyleaks';
+$string['clenablemodulefor'] = 'הפעלת Copyleaks עבור {$a}';
+$string['claccountconfig'] = "תצורת חשבון Copyleaks";
+$string['clapiurl'] = 'כתובת API של Copyleaks';
+$string['claccountkey'] = "מפתח Copyleaks";
+$string['claccountsecret'] = "סיסמת Copyleaks";
+$string['clallowstudentaccess'] = 'אפשר לסטודנטים גישה לדוחות גניבה ספרותית';
+$string['clinvalidkeyorsecret'] = 'מפתח או סיסמה לא תקינים';
+$string['clfailtosavedata'] = 'כישלון בשמירת נתוני Copyleaks';
+$string['clplagiarised'] = 'ציון דמיון';
+$string['clresubmitfailed'] = 'נסה שוב';
+$string['clresubmitfailedscans'] = 'שלח מחדש סריקות שנכשלו';
+$string['clresubmitfailedscansdisabled'] = 'שלח מחדש סריקות שנכשלו (זמין פעם בשעה)';
+$string['clopenreport'] = 'לחץ לפתיחת דוח Copyleaks';
+$string['clscoursesettings'] = 'הגדרות Copyleaks';
+$string['clupdateerror'] = 'שגיאה בעת ניסיון לעדכן רשומות במסד הנתונים';
+$string['clinserterror'] = 'שגיאה בעת ניסיון להכניס רשומות למסד הנתונים';
+$string['clsendqueuedsubmissions'] = "תוסף גניבה ספרותית של Copyleaks - טיפול בקבצים בהמתנה";
+$string['clsendresubmissionsfiles'] = "תוסף גניבה ספרותית של Copyleaks - טיפול בתוצאות שהוגשו מחדש";
+$string['clsyncoriginalityscore'] = "תוסף גניבה ספרותית של Copyleaks - סנכרון ציון מקוריות";
+$string['clsyncconfigtask'] = "תוסף גניבה ספרותית של Copyleaks - סנכרון הגדרות";
+$string['clsendrequestqueue'] = "תוסף גניבה ספרותית של Copyleaks - טיפול בבקשות בהמתנה מחדש";
+$string['clupserteulausers'] = "תוסף גניבה ספרותית של Copyleaks - טיפול בהסכמות משתמשים ל-EULA";
+$string['clbackgroundtask'] = "תוסף גניבה ספרותית של Copyleaks - טיפול במשימות ברקע";
+$string['clupdatereportscores'] = "תוסף גניבה ספרותית של Copyleaks - עדכון ציון דמיון בבדיקת גניבה ספרותית";
+$string['clduplicatecoursemodules'] = "תוסף גניבה ספרותית של Copyleaks - טיפול בהעתקת מודולים בקורס";
+$string['cldraftsubmit'] = "שלח קבצים רק כאשר הסטודנטים לוחצים על כפתור ההגשה";
+$string['cldraftsubmit_help'] = "אפשרות זו זמינה רק אם 'חובה על הסטודנטים ללחוץ על כפתור ההגשה' מוגדר כן";
+$string['clreportgenspeed'] = 'מתי ליצור את הדוח?';
+$string['clgenereportimmediately'] = 'צור דוחות מיד';
+$string['clgenereportonduedate'] = 'צור דוחות בתאריך ההגשה';
+$string['cltaskfailedconnecting'] = 'לא ניתן ליצור חיבור ל-Copyleaks, שגיאה: {$a}';
+$string['clapisubmissionerror'] = 'Copyleaks החזיר שגיאה בעת ניסיון לשלוח קובץ לבדיקה - ';
+$string['clcheatingdetected'] = 'זוהה ניסיון רמאות, פתח את הדוח למידע נוסף';
+$string['clcheatingdetectedtxt'] = 'זוהה ניסיון רמאות';
+$string['clreportpagetitle'] = 'דוח Copyleaks';
+$string['clscansettingspagebtntxt'] = 'ערוך הגדרות סריקה';
+$string['clanalyticsbtntxt'] = 'צפה באנליטיקות';
+$string['clmodulescansettingstxt'] = "ערוך הגדרות סריקה";
+$string['cldisablesettingstooltip'] = "מתבצע סנכרון נתונים ל-Copyleaks...";
+$string['clopenfullscreen'] = 'פתח במסך מלא';
+$string['cllogsheading'] = 'יומנים';
+$string['clpoweredbycopyleaks'] = 'מופעל על ידי Copyleaks';
+$string['clplagiarisefailed'] = 'נכשל';
+$string['clplagiarisescanning'] = 'מתבצע סריקה לזיהוי גניבה ספרותית...';
+$string['clplagiarisequeued'] = 'מתוזמן לבדיקה על גניבה ספרותית בתאריך {$a}';
+$string['cldisabledformodule'] = 'תוסף Copyleaks מושבת עבור מודול זה.';
+$string['clnopageaccess'] = 'אין לך גישה לעמוד זה.';
+$string['clplagiarismdetectionthresholds'] = 'ספי זיהוי גניבה ספרותית';
+$string['claicontentdetectionthresholds'] = 'ספי זיהוי תוכן מבוסס בינה מלאכותית';
+$string['cllowcontentmatchthreshold'] = 'חומרה נמוכה:';
+$string['clmidcontentmatchthreshold'] = 'חומרה בינונית:';
+$string['clhighcontentmatchthreshold'] = 'חומרה גבוהה:';
+$string['clplagiarismdetectionthresholds_help'] = '<b>קידוד צבעים לחומרת גניבה ספרותית:</b>  
+<ul>  
+<li><span style="color:green;"><b>ירוק:</b></span> חומרה נמוכה.</li>  
+<li><span style="color:orange;"><b>צהוב:</b></span> חומרה בינונית.</li>  
+<li><span style="color:red;"><b>אדום:</b></span> חומרה גבוהה.</li>  
+</ul> ';
+$string['claicontentdetectionthresholds_help'] = '<b>קידוד צבעים לחומרת תוכן מבוסס בינה מלאכותית:</b>  
+<ul>  
+<li><span style="color:green;"><b>ירוק:</b></span> חומרה נמוכה.</li>  
+<li><span style="color:orange;"><b>צהוב:</b></span> חומרה בינונית.</li>  
+<li><span style="color:red;"><b>אדום:</b></span> חומרה גבוהה.</li>  
+</ul> ';
+
+
+$string['claicontentscore'] = 'ציון תוכן מבוסס בינה מלאכותית';
+$string['clplagiarismscore'] = 'ציון גניבה ספרותית';
+$string['clwritingfeedbackissues'] = 'תיקוני כתיבה';
+$string['clscanfailedbtn'] = 'הסריקה נכשלה';
+$string['cltryagainbtn'] = 'נסה שוב';
+$string['cltryagainmsg'] = 'שלח מחדש לסריקת Copyleaks';
+$string['clscaninprogress'] = 'הסריקה בעיצומה...';
+$string['clscheduledintime'] = 'מתוזמן: {$a}';
+$string['claicontentscheduled'] = 'סריקת תוכן מבוסס בינה מלאכותית';
+$string['clplagiarismcontentscheduled'] = 'סריקת גניבה ספרותית';
+$string['clwritingfeedbackcontentscheduled'] = 'תיקוני כתיבה';
+$string['cldownloadreport'] = 'הורדת דוח PDF';
+$string['clopenreport'] = 'פתיחת עמוד הדוח';
+$string['clcopyreporturl'] = 'העתקת קישור לעמוד הדוח ללוח העריכה';
+$string['cltimesoon'] = 'בקרוב';
+$string['cltimeminutes'] = 'דקות';
+$string['cltimehours'] = 'שעות';
+$string['cltimedays'] = 'ימים';
+$string['cltimemonths'] = 'חודשים';
+$string['cltimemin'] = 'בעוד';
+$string['clpendingduplication'] = 'העתקה בהמתנה';
+$string['clfailedduplication'] = 'העתקה נכשלה';
+
+$string['clreportupdatefailed'] = 'נכשל עדכון הדוח';
+$string['clsendnotificationfailed'] = 'נכשל שליחת ההודעה';
+$string['filenotfound'] = 'הקובץ לא נמצא';
+
+$string['privacy:metadata:core_files'] = 'Copyleaks מאחסן קבצים שהועלו ל-Moodle לצורך שליחת בדיקה ל-Copyleaks.';
+$string['privacy:metadata:plagiarism_copyleaks_files'] = 'מידע המקשר בין הגשה ב-Moodle להגשה ב-Copyleaks.';
+$string['privacy:metadata:plagiarism_copyleaks_files:userid'] = 'מזהה המשתמש שהוא הבעלים של ההגשה.';
+$string['privacy:metadata:plagiarism_copyleaks_files:submitter'] = 'מזהה המשתמש שביצע את ההגשה.';
+$string['privacy:metadata:plagiarism_copyleaks_files:similarityscore'] = 'ציון הדמיון של ההגשה.';
+$string['privacy:metadata:plagiarism_copyleaks_files:lastmodified'] = 'חותמת זמן המציינת מתי המשתמש ערך לאחרונה את ההגשה.';
+$string['privacy:metadata:plagiarism_copyleaks_client'] = 'על מנת לשלב את Copyleaks, יש להחליף נתוני משתמש מסוימים עם Copyleaks.';
+$string['privacy:metadata:plagiarism_copyleaks_client:module_id'] = 'מזהה המודול נשלח ל-Copyleaks למטרות זיהוי.';
+$string['privacy:metadata:plagiarism_copyleaks_client:module_name'] = 'שם המודול נשלח ל-Copyleaks למטרות זיהוי.';
+$string['privacy:metadata:plagiarism_copyleaks_client:module_type'] = 'סוג המודול נשלח ל-Copyleaks למטרות זיהוי.';
+$string['privacy:metadata:plagiarism_copyleaks_client:module_creationtime'] = 'זמן יצירת המודול נשלח ל-Copyleaks למטרות זיהוי.';
+$string['privacy:metadata:plagiarism_copyleaks_client:submittion_userId'] = 'מזהה המשתמש ששלח את ההגשה נשלח ל-Copyleaks למטרות זיהוי.';
+$string['privacy:metadata:plagiarism_copyleaks_client:submittion_name'] = 'שם ההגשה נשלח ל-Copyleaks למטרות זיהוי.';
+$string['privacy:metadata:plagiarism_copyleaks_client:submittion_type'] = 'סוג ההגשה נשלח ל-Copyleaks למטרות זיהוי.';
+$string['privacy:metadata:plagiarism_copyleaks_client:submittion_content'] = 'תוכן ההגשה נשלח ל-Copyleaks לצורך עיבוד הסריקה.';
+
