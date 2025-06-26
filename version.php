934c9fe0b59f644d0bcd1e239557869cdce4ea9c
Shade Amasha <shadea@copyleaks.com>
2025-06-26 10:54:28 +0300

    LMS-1570-fix-alerts-on-moodle-and-save-moodle-version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2025051100;
+$plugin->version = 2025060500;
 $plugin->release = "3.5+";
 $plugin->requires = 2018051700;
 $plugin->component = 'plagiarism_copyleaks';
